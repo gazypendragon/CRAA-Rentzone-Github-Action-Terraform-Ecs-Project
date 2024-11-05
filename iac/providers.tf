58fd208684887a1480bd43b842f35d7caa220171
choh ndefru <ndefrutitus22@gmail.com>
2024-11-05 10:14:10 -0500

    Creating aws resources

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,6 +1,23 @@
-# configure aws provider to establish a secure connection between terraform and aws
+# # configure aws provider to establish a secure connection between terraform and aws
+# provider "aws" {
+#   region = var.region
+
+#   default_tags {
+#     tags = {
+#       "Automation"  = "terraform"
+#       "Project"     = var.project_name
+#       "Environment" = var.environment
+#     }
+#   }
+# }
+
 provider "aws" {
   region = var.region
+
+  assume_role {
+    role_arn     = "arn:aws:iam::131975964823:role/terraform-assume-role"
+    session_name = "CodeBuildSession"
+  }
 
   default_tags {
     tags = {
