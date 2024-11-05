1d3605531a3461dbef041c7ce157a514e3a2aa6f
choh ndefru <ndefrutitus22@gmail.com>
2024-11-05 12:04:48 -0500

    oicd

---
+++ iac/iam_role.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "github_oidc_role" {
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::131975964823:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:gazypendragon/CRAA-Rentzone-Github-Action-Terraform-Ecs-Project:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_oidc_role_policy" {
+  role       = aws_iam_role.github_oidc_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"  # Replace with a more restrictive policy if needed
+}
