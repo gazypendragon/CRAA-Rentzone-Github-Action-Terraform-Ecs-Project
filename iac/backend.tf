275cea24dc4488ad0588e8487f6cb50fcd1b50c1
choh ndefru <ndefrutitus22@gmail.com>
2024-11-06 09:24:06 -0500

    B

--- iac/backend.tf
+++ iac/backend.tf
@@ -5,6 +5,6 @@ terraform {
     key            = "rentzone-app/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
-    assume_role_arn = "arn:aws:iam::131975964823:role/GitHubActionsRole"  # Replace with your GitHub OIDC role ARN
+    # assume_role_arn = "arn:aws:iam::131975964823:role/GitHubActionsRole"  # Replace with your GitHub OIDC role ARN
   }
 }
