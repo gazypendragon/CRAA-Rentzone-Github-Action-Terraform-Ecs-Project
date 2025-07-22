06f91faead78aab90ef7bea87aa7726bc912e375
choh ndefru <ndefrutitus22@gmail.com>
2025-07-22 00:00:36 -0400

    "Reverting to working commit 58fd208 and updating AWS credentials"

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,23 +1,6 @@
-# # configure aws provider to establish a secure connection between terraform and aws
-# provider "aws" {
-#   region = var.region
-
-#   default_tags {
-#     tags = {
-#       "Automation"  = "terraform"
-#       "Project"     = var.project_name
-#       "Environment" = var.environment
-#     }
-#   }
-# }
-
+# configure aws provider to establish a secure connection between terraform and aws
 provider "aws" {
   region = var.region
-
-  assume_role {
-    role_arn     = "arn:aws:iam::131975964823:role/terraform-assume-role"
-    session_name = "CodeBuildSession"
-  }
 
   default_tags {
     tags = {
@@ -27,3 +10,20 @@ provider "aws" {
     }
   }
 }
+
+# provider "aws" {
+#   region = var.region
+
+#   assume_role {
+#     role_arn     = "arn:aws:iam::131975964823:role/terraform-assume-role"
+#     session_name = "CodeBuildSession"
+#   }
+
+#   default_tags {
+#     tags = {
+#       "Automation"  = "terraform"
+#       "Project"     = var.project_name
+#       "Environment" = var.environment
+#     }
+#   }
+# }
